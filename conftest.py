8db9ac59ae81066447141b74a07c85f995c41f69
Vika Kazak <viktoria.kazak0@gmail.com>
2020-05-21 15:05:02 +0300

    files added

---
+++ conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en")
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()

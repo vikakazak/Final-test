23958ae057806d802268f6493008a57dba5f0625
Vika Kazak <viktoria.kazak0@gmail.com>
2020-05-26 15:28:46 +0300

    added assert statement

--- test_main_page.py
+++ test_main_page.py
@@ -4,3 +4,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()

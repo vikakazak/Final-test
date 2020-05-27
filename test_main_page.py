4e129853a57b93dd9423de0fa6ab9711cbf9d279
Vika Kazak <viktoria.kazak0@gmail.com>
2020-05-27 15:20:55 +0300

    added transition between pages

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,9 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()          # выполняем метод страницы - переходим на страницу логина
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)

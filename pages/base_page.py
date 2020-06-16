9b19dd281ed935d4412469bd50be0868e3f70b63
Vika Kazak <viktoria.kazak0@gmail.com>
2020-06-16 12:53:53 +0300

    deleted extra method __init__

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,16 +6,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import math
 class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
-    def open(self):
-        self.browser.get(self.url)
-
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        self.browser.get(self.url)
 
     def is_element_present(self, how, what):
         try:
